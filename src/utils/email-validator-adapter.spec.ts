2cb500a948998c39df2be9430650ea6acf3717d1
Rubens Junior <rubensojunior6@gmail.com>
2021-09-05 00:08:50 -0300

    test: add email validator with validator

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,36 @@
+import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+import { internet } from 'faker'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
+describe('EmailValidator Adapter', () => {
+  test('should return false if validator returns false', () => {
+    const sut = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid(internet.email())
+    expect(isValid).toBe(false)
+  })
+
+  test('should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid(internet.email())
+    expect(isValid).toBe(true)
+  })
+
+  test('should call validator with correct email', () => {
+    const sut = makeSut()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    const email = internet.email()
+    sut.isValid(email)
+    expect(isEmailSpy).toHaveBeenCalledWith(email)
+  })
+})
